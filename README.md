75e63fa4501d3285a721d23a08bfb33f04bac9c9
Sarthakischauhan <sarthakchauhann@gmail.com>
2024-09-24 14:31:47 -0700

    Batman commit with lab1 assignent

---
+++ README.md
@@ -0,0 +1,6 @@
+# VeriVeri
+
+Frontend Developer tries verilog, what could go wrong? These are some of the sample scripts that I have written for my computer architecture class. I will also be experimenting with the beautiful language of Verilog. Hope to make an ALU soon!.
+
+
+Godspeed!
---
+++ logic_bit32.v
@@ -0,0 +1,46 @@
+// 32-bit NOR
+module NOR32_2x1(Y,A,B);
+//output 
+output [31:0] Y;
+//input
+input [31:0] A;
+input [31:0] B;
+
+nor(Y,A,B);
+
+endmodule
+
+// 32-bit AND
+module AND32_2x1(Y,A,B);
+//output 
+output [31:0] Y;
+//input
+input [31:0] A;
+input [31:0] B;
+
+and(Y,A,B);
+
+endmodule
+
+// 32-bit inverter
+module INV32_1x1(Y,A);
+//output 
+output [31:0] Y;
+//input
+input [31:0] A;
+
+not(Y,A);
+
+endmodule
+
+// 32-bit OR
+module OR32_2x1(Y,A,B);
+//output 
+output [31:0] Y;
+//input
+input [31:0] A;
+input [31:0] B;
+
+or(Y,A,B);
+
+endmodule
